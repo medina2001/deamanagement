f975e37d8d49a915f4b64b629c4177cc88f0d671
Gabriel de Oliveira Maciel <gabrieldeoliveiramaciel@eldorados-MacBook-Pro-2.local>
2023-01-17 19:11:01 -0300

    Initial commit

---
+++ src/test/java/br/com/dea/management/deamanagement/DeamanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dea.management.deamanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeamanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
